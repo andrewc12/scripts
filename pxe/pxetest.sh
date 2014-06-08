09ac306d8737b1b271d7ea12d603c530685060c4
Andrew Innes <andrew.c12@gmail.com>
2014-06-08 12:45:57 +0800

    incorporate plop installation into the main script

--- pxe/pxetest.sh
+++ pxe/pxetest.sh
@@ -173,8 +173,32 @@ fi
 #################################################################################################################   
 if [ "$INSTALLPLOP" -eq 1 ]
 then
-    cd
-    ./installplop.sh
+    cd /tmp
+    wget -c  http://download.plop.at/files/bootmngr/plpbt-5.0.15-test.zip
+    unzip plpbt-5.0.15-test.zip
+    mv plpbt-5.0.15-test $TFTPPATH/images/plop
+    cat >> $PXELINUXMENU << EOF
+MENU BEGIN Plop
+MENU LABEL Plop
+MENU TITLE Plop boot loader
+LABEL Back
+MENU EXIT
+MENU LABEL Back
+LABEL Plop Live
+kernel images/plop/plpbt.bin
+MENU LABEL Plop
+TEXT HELP
+Run Plop
+ENDTEXT
+LABEL Plop Install
+kernel images/plop/install/plpinstc.com
+MENU LABEL Install Plop
+TEXT HELP
+Run Plop Install
+ENDTEXT
+MENU END
+EOF
+
 fi
 ##############################################################################################################
 if [ "$INSTALLDEBIANAMD64" -eq 1 ]
