19d24b76a1c3155cca803f629dec471dbe187feb
Andrew Innes <andrew.c12@gmail.com>
2015-08-03 14:28:57 +0800

    Actually changes the configuration file now

--- setupsquid3.sh
+++ setupsquid3.sh
@@ -1,11 +1,14 @@
-aptitude install squid3 squid3-common
+#!/bin/bash
+apt-get install squid3 squid3-common
 
 
-nano /etc/squid3/squid.conf
+#nano /etc/squid3/squid.conf
+#sed -i '/TEXT in line to be replaced/c\new contents' /tmp/foo
+#sed 's/.*TEXT_TO_BE_REPLACED.*/This line is removed by the admin./'
+sed -i 's!.*acl localnet src 10.0.0.0/8.*!acl localnet src 10.0.0.0/8!' /etc/squid3/squid.conf
 
-acl localnet src 10.0.0.0/24
+sed -i 's!.*http_access allow localhost.*!http_access allow localhost!' /etc/squid3/squid.conf
+sed -i 's!.*http_access allow localnet.*!http_access allow localnet!' /etc/squid3/squid.conf
 
-http_access allow localhost
-http_access allow localnet
 
 /etc/init.d/squid3 restart
